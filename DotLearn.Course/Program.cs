90683a54a7a4c362f316d4268f8e28f2b38c135e
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2026-04-18 13:03:12 +0530

    feat(internal): Implement centralized CorrelationIdDelegatingHandler for distributed tracing

--- DotLearn.Course/Program.cs
+++ DotLearn.Course/Program.cs
@@ -27,6 +27,8 @@ if (!builder.Environment.IsDevelopment())
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddTransient<DotLearn.Course.Middleware.CorrelationIdDelegatingHandler>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
